684a6ceea89bf1aad8071a20bd91425f26d8a77f
karimy <karim.alrashdan14@gmail.com>
2022-11-30 00:14:09 -0700

    Link testing and Card files to Deck-test file

---
+++ test/Deck-test.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const chai = require('chai');
+const expect = chai.expect;
+
+const data = require('../src/data-samples')
+const Card = require('../src/Card')
+
+describe('Deck', () => {
+
+})
+/*Your Deck class should be initialized with an array of Card objects and should have an accompanying test file. It should know how many Cards are in the Deck. */
