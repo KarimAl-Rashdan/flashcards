342795f892b8c2df28de4ae81ca3f27c8927841a
karimy <karim.alrashdan14@gmail.com>
2022-11-30 00:18:29 -0700

    Change prototypeSampleQuestions to prototypeSampleCards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,9 +3,12 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const data = require('../src/data-samples')
+const dataSampleCards = data.prototypeSampleData
 const Card = require('../src/Card')
 
 describe('Deck', () => {
 
+const allCards = dataSample
+console.log(allCards)
 })
 /*Your Deck class should be initialized with an array of Card objects and should have an accompanying test file. It should know how many Cards are in the Deck. */
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,7 +6,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const data = require('../src/data-samples')
-const protoTypeSampleQuestions = data.prototypeSampleData
+const protoTypeSampleCards = data.prototypeSampleData
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
@@ -14,7 +14,7 @@ describe('Turn', function() {
   
   let turn1;
   let turn2;
-  let allCards = protoTypeSampleQuestions.map(card => { 
+  let allCards = protoTypeSampleCards.map(card => { 
     return new Card(card.id, card.question, card.answers, card.correctAnswer)
     
   })
